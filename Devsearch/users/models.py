9a524174bf86f6812c234c4558a03fd66ed8d257
BarbareGogritchiani <gogrichianibarbare@gmail.com>
2025-04-21 22:12:07 +0400

    Added signals

--- Devsearch/users/models.py
+++ Devsearch/users/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 import uuid
-# Create your models here.
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE,null=True,blank=True) # anytime the user is deleted , profile is deleted too
@@ -21,7 +20,7 @@ class Profile(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
 
     def __str__(self):
-        return self.user.username
+        return self.username
 
 
 
